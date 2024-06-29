cfeb7cafa12926f0c4200bf2408b5ee6d7cf3aed
Youssef El Idrissi <professionalidrissi@gmail.com>
2024-06-29 23:55:47 +0100

    added: test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module 0xw3ston"""
+import unittest
+import console
+
+
+class test_Console(unittest.TestCase):
+    """doc 0xw3ston"""
+
+    def test_documentation(self):
+        """doc 0xw3ston"""
+        self.assertIsNotNone(console.__doc__)
